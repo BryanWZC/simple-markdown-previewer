8b84b61332d06591dc94bbfa172553c85f5ea0f4
Bryan Wong <bryanwongzhicheng@gmail.com>
2020-06-06 14:13:51 -0400

    Updates

---
+++ precache-manifest.0e12fe3c6fe35ba75032cc126844d170.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d7713c4b5f71d0e1ff8d9ab04eee3e",
+    "url": "/simple-markdown-previewer/index.html"
+  },
+  {
+    "revision": "164808d203605ac21652",
+    "url": "/simple-markdown-previewer/static/css/main.49e43d1a.chunk.css"
+  },
+  {
+    "revision": "030ce4e6d81a6772162f",
+    "url": "/simple-markdown-previewer/static/js/2.2b55ee28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-markdown-previewer/static/js/2.2b55ee28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "164808d203605ac21652",
+    "url": "/simple-markdown-previewer/static/js/main.1c1f4829.chunk.js"
+  },
+  {
+    "revision": "f3b3c7e0bc8ab5dbbf8a",
+    "url": "/simple-markdown-previewer/static/js/runtime-main.67927c25.js"
+  }
+]);
